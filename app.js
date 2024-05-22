f62daaf0deb99eac964659ef5221f6a969a180a3
Amos Burton <157778700+theamosburton@users.noreply.github.com>
2024-05-21 23:31:37 -0700

    sdfdsf

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+app.get('/account', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'account.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/files/account.js
@@ -0,0 +1,29 @@
+function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    // You can send this token to your server for further processing
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
+        callback: handleCredentialResponse
+    });
+    document.getElementById('loginButton').addEventListener('click', function () {
+        google.accounts.id.prompt();
+    });
+};
+
+function toggleMenus(){
+    var showMenus = document.getElementById('expandMenusIcon');
+    var hideMenus = document.getElementById('hideMenusIcon');
+    var hiddenMenus = document.getElementById('hiddenMenus');
+    if(hideMenus.style.display == 'none'){
+        hideMenus.style.display = 'flex';
+        hiddenMenus.style.display = 'flex';
+        showMenus.style.display = 'none';
+    }else{
+        hideMenus.style.display = 'none';
+        hiddenMenus.style.display = 'none';
+        showMenus.style.display = 'flex';
+    }
+}
