5afe4b81ada186ff9ae6677ac9d8d402e3559ac1
Amos Burton <157778700+theamosburton@users.noreply.github.com>
2024-05-25 02:06:22 -0700

    new commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-// app.js
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
