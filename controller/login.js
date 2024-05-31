ae1ecfec663e3097b16cfa99f46cbd7be985bfc4
Amos Burton <157778700+theamosburton@users.noreply.github.com>
2024-05-31 23:57:16 +0530

    dfdg

--- controller/login.js
+++ controller/login.js
@@ -1,40 +1,6 @@
 const Database = require('./db');
-const cookieOnly = require('cookie');
 var Login = {};
 Login.initiliaze = () =>{
-
-
-class Login{
-    connect = Database.connect;
-    client = Database.client;
-    constructor(data){
-        if (!connect.status) {
-            return {loginStatus:false, log:"Database Connection Error"};
-        }else if(this.checkUserExists(data)){
-            return this.loginUser(data);
-        }else{
-            const addUser = this.addUser(data);
-            if (addUser.status) {
-                return this.loginUser(data);
-            }else{
-                return {loginStatus:false, log:addUser.log};
-            }
-            
-        }
-    }
-
-    checkUserExists(data){
-       // do some stuff
-    }
-
-    addUser(data){
-
-    }
-
-    loginUser(data){
-        return {loginStatus:true, log:"Logged In Successfully"}
-    }
-
 
 }
 
